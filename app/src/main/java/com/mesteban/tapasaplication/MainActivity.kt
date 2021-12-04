eadb27efce0bf6d5764da22215daa063164ce9ef
mesteban10 <mariaesteban1997@gmail.com>
2021-12-04 17:58:02 +0100

    First Commit

---
+++ app/src/main/java/com/mesteban/tapasaplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mesteban.tapasaplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
